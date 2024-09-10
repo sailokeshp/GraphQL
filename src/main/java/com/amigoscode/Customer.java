2401409f4be221309cd870f7a9fa4913eebc6251
lokesh <135771083+parimilokesh@users.noreply.github.com>
2024-09-10 17:37:26 -0400

    main

---
+++ src/main/java/com/amigoscode/Customer.java
@@ -0,0 +1,46 @@
+package com.amigoscode;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Customer {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    // Constructors, getters, and setters
+    public Customer() {}
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
---
+++ src/main/java/com/amigoscode/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.amigoscode;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+}
+
